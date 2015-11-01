795a5eac264f709036090cf7e802fca4e884e1aa
Kevin Reece <kevinjreece@gmail.com>
2015-10-31 19:43:13 -0600

    OnePathDijkstra works.

--- 3-network-routing/Form1.cs
+++ 3-network-routing/Form1.cs
@@ -29,8 +29,8 @@ namespace NetworkRouting
             List<PointF> points = generatePoints(size, rand);
             resetImageToPoints(points);
             this.points = points;
-            startNodeIndex = -1;
-            stopNodeIndex = -1;
+            src = -1;
+            dst = -1;
             sourceNodeBox.Text = "";
             targetNodeBox.Text = "";
         }
@@ -88,28 +88,194 @@ namespace NetworkRouting
         private Graphics graphics;
         private List<HashSet<int>> adjacencyList;
 
-        // Use this to generate routing tables for every node
-        private void solveButton_Click(object sender, EventArgs e)
+        private void TestDrawing()
         {
-            // *** Implement this method, use the variables "startNodeIndex" and "stopNodeIndex" as the indices for your start and stop points, respectively ***
             Pen pen = new Pen(Color.Black);
             for (int i = 0; i < points.Count(); i++)
             {
-                PointF p1 = points.ElementAt(i);
-                for (int j = 0; j < adjacencyList.ElementAt(i).Count(); j++)
+                PointF p1 = points[i];
+                for (int j = 0; j < adjacencyList[i].Count(); j++)
                 {
-                    PointF p2 = points.ElementAt(adjacencyList.ElementAt(i).ElementAt(j));
+                    PointF p2 = points[adjacencyList[i].ElementAt(j)];
                     graphics.DrawLine(pen, p1, p2);
                 }
             }
             pictureBox.Refresh();
+        }
 
-            PriorityQueue.TestPriorityQueue();
+        private void TestDist()
+        {
+            Pen pen = new Pen(Color.Black);
+            Font font = new Font("Arial", 8);
+            SolidBrush brush = new SolidBrush(Color.Black);
+            PointF p0 = points.ElementAt(src);
+            for (int i = 0; i < adjacencyList[src].Count(); i++)
+            {
+                int dest_id = adjacencyList[src].ElementAt(i);
+                PointF p1 = points[dest_id];
+                double dist = GetDist(p0, p1);
+                Console.WriteLine("Distance between node " + src + " " + p0 + " and node " + dest_id + " " + p1 + " is " + dist);
+                graphics.DrawLine(pen, p0, p1);
+                graphics.DrawString(dist.ToString("#.##"), font, brush, GetMidPoint(p0, p1));
+            }
+            pictureBox.Refresh();
+        }
+
+        private double GetDist(PointF p1, PointF p2)
+        {
+            double dx = p1.X - p2.X;
+            double dy = p1.Y - p2.Y;
+            return Math.Sqrt(dx * dx + dy * dy);
+        }
+
+        private PointF GetMidPoint(PointF p1, PointF p2)
+        {
+            PointF left;
+            PointF right;
+            PointF top;
+            PointF bottom;
+
+            if (p1.X < p2.X)
+            {
+                left = p1;
+                right = p2;
+            }
+            else
+            {
+                left = p2;
+                right = p1;
+            }
+
+            if (p1.Y < p2.Y)
+            {
+                top = p1;
+                bottom = p2;
+            }
+            else
+            {
+                top = p2;
+                bottom = p1;
+            }
+
+            return new PointF(left.X + ((right.X - left.X) / 2), top.Y + ((bottom.Y - top.Y) / 2));
+        }
+
+        public void OnePathDijkstra()
+        {
+            Console.WriteLine("OnePathDijkstra");
+            int n_nodes = points.Count();
+            PriorityQueue q = new PriorityQueue(n_nodes);
+            double[] src_dist = new double[n_nodes];
+            int[] prev = new int[n_nodes];
+            int i;
+
+            for (i = 0; i < n_nodes; i++)
+            {
+                src_dist[i] = double.MaxValue;
+                prev[i] = -1;
+            }
+
+            src_dist[src] = 0;
+            prev[src] = -1;
+
+            q.Insert(src, src_dist[src]);
+
+            while (!q.IsEmpty())
+            {
+                int id = q.PopMin();
+                if (id == dst) { break; }
+                HashSet<int> adj_nodes = adjacencyList[id];
+                for (i = 0; i < adj_nodes.Count(); i++)
+                {
+                    int temp_id = adj_nodes.ElementAt(i);
+                    double temp_dist = src_dist[id] + GetDist(points[id], points[temp_id]);
+                    //Console.WriteLine("Distance from " + id + " to " + temp_id + " is " + temp_dist);
+
+                    if (src_dist[temp_id] == double.MaxValue) // If the node has NOT been visited
+                    {
+                        src_dist[temp_id] = temp_dist;
+                        prev[temp_id] = id;
+
+                        if (!q.Insert(temp_id, src_dist[temp_id]))
+                        {
+                            Console.WriteLine("ERROR inserting id " + temp_id + " connected to id " + id);
+                            return;
+                        }
+                        //Console.WriteLine("queue:\n" + q.ToString());
+                    }
+                    else if (temp_dist < src_dist[temp_id]) // If the node HAS been visited and the temp distance is less than the previous distance
+                    {
+                        src_dist[temp_id] = temp_dist;
+                        prev[temp_id] = id;
+
+                        if (!q.ReduceVal(temp_id, temp_dist))
+                        {
+                            Console.WriteLine("ERROR reducing id " + temp_id + " connected to id " + id);
+                            return;
+                        }
+                        //Console.WriteLine("queue:\n" + q.ToString());
+                    }
+                }
+            }
+
+            if (src_dist[dst] != double.MaxValue)
+            {
+                Pen pen = new Pen(Color.Red);
+                int temp = dst;
+                while (prev[temp] != -1)
+                {
+                    graphics.DrawLine(pen, points[temp], points[prev[temp]]);
+                    temp = prev[temp];
+                }
+                pictureBox.Refresh();
+                Console.WriteLine("Distance to destination is " + src_dist[dst]);
+            }
+            else
+            {
+                Console.WriteLine("Destination is unreachable");
+            }
+        }
+
+        public void AllPathDijkstra()
+        {
+
+        }
+
+        // Use this to generate routing tables for every node
+        private void solveButton_Click(object sender, EventArgs e)
+        {
+            // *** Implement this method, use the variables "startNodeIndex" and "stopNodeIndex" as the indices for your start and stop points, respectively ***
+            //TestDrawing();
+            //TestDist();
+
+            OnePathDijkstra();
+
+            
+
+            //PriorityQueue.TestPriorityQueue();
+        }
+
+        private void sourceNodeBox_LostFocus(object sender, EventArgs e)
+        {
+            if (sourceNodeBox.Text == "") { return; }
+
+            src = int.Parse(sourceNodeBox.Text);
+            resetImageToPoints(points);
+            paintStartStopPoints();
+        }
+
+        private void targetNodeBox_LostFocus(object sender, EventArgs e)
+        {
+            if (targetNodeBox.Text == "") { return; }
+
+            dst = int.Parse(targetNodeBox.Text);
+            resetImageToPoints(points);
+            paintStartStopPoints();
         }
 
         private Boolean startStopToggle = true;
-        private int startNodeIndex = -1;
-        private int stopNodeIndex = -1;
+        private int src = -1;
+        private int dst = -1;
         private void pictureBox_MouseDown(object sender, MouseEventArgs e)
         {
             if (points.Count > 0)
@@ -118,12 +284,12 @@ namespace NetworkRouting
                 int index = ClosestPoint(points, mouseDownLocation);
                 if (startStopToggle)
                 {
-                    startNodeIndex = index;
+                    src = index;
                     sourceNodeBox.Text = "" + index;
                 }
                 else
                 {
-                    stopNodeIndex = index;
+                    dst = index;
                     targetNodeBox.Text = "" + index;
                 }
                 startStopToggle = !startStopToggle;
@@ -135,18 +301,18 @@ namespace NetworkRouting
 
         private void paintStartStopPoints()
         {
-            if (startNodeIndex > -1)
+            if (src > -1)
             {
                 Graphics graphics = Graphics.FromImage(pictureBox.Image);
-                graphics.DrawEllipse(new Pen(Color.Green, 6), points[startNodeIndex].X, points[startNodeIndex].Y, 1, 1);
+                graphics.DrawEllipse(new Pen(Color.Green, 6), points[src].X, points[src].Y, 1, 1);
                 this.graphics = graphics;
                 pictureBox.Invalidate();
             }
 
-            if (stopNodeIndex > -1)
+            if (dst > -1)
             {
                 Graphics graphics = Graphics.FromImage(pictureBox.Image);
-                graphics.DrawEllipse(new Pen(Color.Red, 2), points[stopNodeIndex].X - 3, points[stopNodeIndex].Y - 3, 8, 8);
+                graphics.DrawEllipse(new Pen(Color.Red, 2), points[dst].X - 3, points[dst].Y - 3, 8, 8);
                 this.graphics = graphics;
                 pictureBox.Invalidate();
             }
